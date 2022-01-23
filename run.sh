131bf003891856f4a83db6c6663a555ea803f81a
Skotuson <58478268+Skotuson@users.noreply.github.com>
2022-01-23 23:39:18 +0100

    edit run script

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 clear
 g++ -Wall -pedantic "$1" -fsanitize=address -g
-if [ "$#" -eq 2 ]; then
-./a.out < "$2"
+if [ "$#" -eq 3 ]; then {
+    if [ "$2" = "to" ]; then {
+        ./a.out > "$3";
+    }
+    else ./a.out < "$3";
+    fi
+}
 else ./a.out
 fi
 rm a.out
